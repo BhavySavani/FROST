550960303b2f6366aca25f00c9c5ea17f325fa7e
Ali Murabbi <alimurabbi8671@gmail.com>
2025-01-19 23:00:53 +0530

    adding dummy core ip verilog files for testing

---
+++ main/core_ips/RS485/RS485.v
@@ -0,0 +1,8 @@
+module RS485 #(clk_frequency)
+(
+    input clk,
+    input rx,
+    output tx
+);
+
+endmodule
---
+++ main/core_ips/uart/uart.v
@@ -0,0 +1,8 @@
+module uart #(parameter baud_rate,clk_frequency)
+(
+    input rx,
+    input clk,
+    output tx
+);
+
+endmodule 
